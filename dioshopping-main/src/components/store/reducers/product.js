550ce7f0898c205738b83b3e65e9cf4e2a57b9b8
JhonatanNeves <nevesjn2@gmail.com>
2022-02-05 11:17:06 -0300

    Att

---
+++ dioshopping-main/src/components/store/reducers/product.js
@@ -0,0 +1,136 @@
+const productsList = [
+    {
+      id_product: 1,
+      category: "novos",
+      fk_idcategorys: 1,
+      name_product: "Eternos",
+      price: 25.90,
+      image: "./images/produtos/e1.jpg",
+      id_categorys: 1,
+      name_categorys: "Lançamentos",
+      selected: false
+    },
+    {
+      id_product: 2,
+      category: "novos",
+      fk_idcategorys: 1,
+      name_product: "Matrix",
+      price: 60.80,
+      image: "./images/produtos/m2.jpg",
+      id_categorys: 1,
+      name_categorys: "Lançamentos",
+      selected: false
+    },
+    {
+      id_product: 9,
+      category: "novos",
+      fk_idcategorys: 1,
+      name_product: "Pacificador",
+      price: 45.90,
+      image: "./images/produtos/p3.jpg",
+      id_categorys: 1,
+      name_categorys: "Lançamentos",
+      selected: false
+    },
+    {
+      id_product: 4,
+      category: "serie",
+      fk_idcategorys: 2,
+      name_product: "Grey's Anatomy",
+      price: 99.99,
+      image: "./images/produtos/a4.jpg",
+      id_categorys: 2,
+      name_categorys: "Séries",
+      selected: false
+    },
+    {
+      id_product: 10,
+      category: "serie",
+      fk_idcategorys: 2,
+      name_product: "Peaky Blinders",
+      price: 70.68,
+      image: "./images/produtos/p5.jpg",
+      id_categorys: 2,
+      name_categorys: "Séries",
+      selected: false
+    },
+    {
+      id_product: 11,
+      category: "fimes",
+      fk_idcategorys: 3,
+      name_product: "Desejo Sombrio",
+      price: 45.60,
+      image: "./images/produtos/d6.jpg",
+      id_categorys: 3,
+      name_categorys: "Filmes",
+      selected: false
+    },
+    {
+      id_product: 6,
+      category: "filmes",
+      fk_idcategorys: 3,
+      name_product: "Indecente",
+      price: 67.60,
+      image: "./images/produtos/i7.jpg",
+      id_categorys: 3,
+      name_categorys: "Filmes",
+      selected: false
+    },
+    {
+      id_product: 7,
+      category: "series",
+      fk_idcategorys: 2,
+      name_product: "The Walking Dead",
+      price: 88.70,
+      image: "./images/produtos/t8.jpg",
+      id_categorys: 2,
+      name_categorys: "Séries",
+      selected: false
+    },
+    {
+      id_product: 3,
+      category: "anime",
+      fk_idcategorys: 4,
+      name_product: "Demon Slayer",
+      price: 66.40,
+      image: "./images/produtos/d9.jpg",
+      id_categorys: 4,
+      name_categorys: "Animes",
+      selected: false
+    },
+    {
+      id_product: 5,
+      category: "series",
+      fk_idcategorys: 2,
+      name_product: "Gavião Aqueiro",
+      price: 45.80,
+      image: "./images/produtos/g10.jpg",
+      id_categorys: 2,
+      name_categorys: "Séries",
+      selected: false
+    },
+    {
+      id_product: 8,
+      category: "filmes",
+      fk_idcategorys: 3,
+      name_product: "Mãe x Androides",
+      price: 55.50,
+      image: "./images/produtos/m11.jpg",
+      id_categorys: 3,
+      name_categorys: "Filmes",
+      selected: false
+    },
+    {
+      id_product: 12,
+      category: "filmes",
+      fk_idcategorys: 3,
+      name_product: "Pandemia",
+      price: 58.90,
+      image: "./images/produtos/p12.jpg",
+      id_categorys: 3,
+      name_categorys: "Filmes",
+      selected: false
+    },
+];
+
+export default productsList;
